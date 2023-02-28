64a363eb1fcb18286aaa157e039045d680eb1e47
kohj1018 <kohj1018@hanyang.ac.kr>
2023-02-28 15:41:32 +0900

    [add] Bottom Navigation Bar 구현

--- lib/screen/webview_screen.dart
+++ lib/screen/webview_screen.dart
@@ -13,33 +13,40 @@ class WebViewScreen extends StatefulWidget {
 
 class _WebViewScreenState extends State<WebViewScreen> {
   late final WebViewController controller;
+  int _selectedIndex = 0;
 
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    controller.runJavaScript('window.changePage($index)');
+  }
 
   @override
   void initState() {
     super.initState();
 
     controller = WebViewController()
-    ..setJavaScriptMode(JavaScriptMode.unrestricted)
-    ..setBackgroundColor(const Color(0x00000000))
-    ..setNavigationDelegate(
-      NavigationDelegate(
-        onProgress: (int progress) {},
-        onPageStarted: (String url) {},
-        onPageFinished: (String url) {},
-        onWebResourceError: (WebResourceError error) {},
-        onNavigationRequest: (NavigationRequest request) {
-          // if (request.url.startsWith('https://www.youtube.com/')) {
-          //   return NavigationDecision.prevent;
-          // }
-          return NavigationDecision.navigate;
-        }),
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+            onProgress: (int progress) {},
+            onPageStarted: (String url) {},
+            onPageFinished: (String url) {},
+            onWebResourceError: (WebResourceError error) {},
+            onNavigationRequest: (NavigationRequest request) {
+              // if (request.url.startsWith('https://www.youtube.com/')) {
+              //   return NavigationDecision.prevent;
+              // }
+              return NavigationDecision.navigate;
+            }),
       )
       ..setUserAgent(Platform.isIOS
           ? 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_1_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.1 Mobile/15E148 Safari/604.1'
           : 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Mobile Safari/537.36')
       ..enableZoom(false)
-      ..loadRequest(Uri.parse('https://somapeople.kr'));
+      ..loadRequest(Uri.parse('https://www.somapeople.kr'));
   }
 
   @override
@@ -48,37 +55,59 @@ class _WebViewScreenState extends State<WebViewScreen> {
       onWillPop: () {
         var future = controller.canGoBack();
         future.then((canGoBack) => {
-          if (canGoBack) {
-            controller.goBack()
-          } else {
-            showDialog(
-              context: context,
-              builder: (context) => AlertDialog(
-                title: Text('앱 종료'),
-                content: Text('소마인을 정말 종료하시겠어요? 😥'),
-                actions: [
-                  TextButton(
-                    onPressed: () {
-                      SystemNavigator.pop();
-                    },
-                    child: Text('예'),
+              if (canGoBack)
+                {controller.goBack()}
+              else
+                {
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: const Text('앱 종료'),
+                      content: const Text('소마인을 정말 종료하시겠어요? 😥'),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            SystemNavigator.pop();
+                          },
+                          child: const Text('예'),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text('아니오'),
+                        ),
+                      ],
+                    ),
                   ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: Text('아니오'),
-                  ),
-                ],
-              ),
-            ),
-          }
-        });
+                }
+            });
         return Future.value(false);
       },
       child: SafeArea(
         child: Scaffold(
           body: WebViewWidget(controller: controller),
+          bottomNavigationBar: BottomNavigationBar(
+            backgroundColor: Colors.white,
+            selectedItemColor: const Color(0xff374151),
+            unselectedItemColor: const Color(0xffE5E7EB),
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: '홈',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.article),
+                label: '게시판',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: '프로필',
+              ),
+            ],
+            currentIndex: _selectedIndex,
+            onTap: _onItemTapped,
+          ),
         ),
       ),
     );
