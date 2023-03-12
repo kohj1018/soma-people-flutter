f3de8e7e8ced2368ee01f273de4fd604465e984a
kohj1018 <kohj1018@hanyang.ac.kr>
2023-03-13 02:36:51 +0900

    [fix] android userAgent 수정 (ë로그인 시 안전하지 ã 않은 브라우저 문제 해결)

--- lib/screen/webview_screen.dart
+++ lib/screen/webview_screen.dart
@@ -92,7 +92,7 @@ class _WebViewScreenState extends State<WebViewScreen> {
       )
       ..setUserAgent(Platform.isIOS
           ? 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_1_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.1 Mobile/15E148 Safari/604.1'
-          : 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Mobile Safari/537.36')
+          : 'Mozilla/5.0 (Linux; Android 10; Redmi Note 8 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.88 Mobile Safari/537.36')
       ..enableZoom(false)
       ..loadRequest(Uri.parse('https://www.somapeople.kr'));
 
